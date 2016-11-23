c377597b1565777f0546baafd32dcd168a184aec
liammachado <liammachado@users.noreply.github.com>
2016-11-23 13:33:00 -0700

    Update mainwindow.cpp
    Connected login and have an account buttons to change the pane.

--- portal/mainwindow.cpp
+++ portal/mainwindow.cpp
@@ -6,6 +6,8 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    connect(ui->createAccountButton, &QPushButton::pressed, this, [this] { ui->worldTreeWidget->setCurrentIndex(1); });
+    connect(ui->haveAnAccountButton, &QPushButton::pressed, this, [this] { ui->worldTreeWidget->setCurrentIndex(0); });
 }
 
 MainWindow::~MainWindow()

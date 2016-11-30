183d6afbe7b7b3215891149f6a915b04ad974868
Alex Steele <steeleal123@gmail.com>
2016-11-30 10:45:45 -0700

    game-client: Fix world's includes.

--- game-client/src/world.h
+++ game-client/src/world.h
@@ -1,20 +1,19 @@
 #ifndef LQ_WORLD_H_
 #define LQ_WORLD_H_
 
-#include "entity.h"
-#include "physicalworld.h"
-#include "playerstats.h"
 #include <SFML/Graphics.hpp>
 #include <SFML/OpenGL.hpp>
 #include <SFML/Window.hpp>
 #include <string>
 #include <vector>
+#include "entity.h"
+#include "physicalworld.h"
+#include "playerstats.h"
 
 #define TIMESTEP 1.f/60.f
 
 class World {
 private:
-// vector probably isn't the most efficient way to store the world...
     std::vector<Entity*> entities;
     double timer;
     double stutterLength;

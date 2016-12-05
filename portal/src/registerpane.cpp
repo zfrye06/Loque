315e404fafc99f270093c3e1dcf65bd1a22d151a
liammachado <liam.machado@yahoo.com>
2016-12-05 11:20:26 -0700

    Added bunch of registration stuff.

--- portal/src/registerpane.cpp
+++ portal/src/registerpane.cpp
@@ -9,30 +9,82 @@ RegisterPane::RegisterPane(QWidget *parent) :
 
     connect(ui->registerButton, &QPushButton::clicked,
             this, &RegisterPane::attemptRegistration);
+    connect(ui->adminCheck, &QCheckBox::stateChanged, this, &RegisterPane::adminBoxChanged);
+    connect(ui->returnToLoginButton, &QPushButton::clicked, this, &RegisterPane::returnToLogin);
 }
 
-void RegisterPane::attemptRegistration() {
-    ui->errorLabel->clear();
-
-    std::string username = ui->usernameField->text().toStdString();
-    std::string password = ui->passwordField->text().toStdString();
-    UserType userType = ui->adminCheck->isChecked() ? UserType::ADMIN : UserType::STUDENT;
-    if (username.length() == 0 || password.length() == 0) {
-        ui->errorLabel->setText("Oops! Be sure to enter a username and password."); 
-        return;
-    }
-    LoqueClient client;
-    LoginResult login;
-    auto status = client.createAccount(username, password, userType, login);
-    if (status != Status::OK || login.userType == UserType::DNE) {
-        if (status == Status::NETWORK_ERR) {
-            ui->errorLabel->setText("Oops! Looks like we can't connect to the network.");
-        } else {
-            ui->errorLabel->setText("Oops! Looks like that username has been used before.");
+void RegisterPane::attemptRegistration()
+{
+    if (ui->usernameField->text().size() == 0) {
+        displayErrorMessage("Username cannot be blank.");
+    } else if (ui->usernameField->text().size() < 6 || ui->usernameField->text().size() > 20) {
+        displayErrorMessage("Username must be between six and twenty characters long.");
+    } else if (ui->passwordField->text().size() == 0) {
+        displayErrorMessage("Password cannot be blank.");
+    } else if (ui->passwordField->text().size() < 6 || ui->usernameField->text().size() > 20) {
+        displayErrorMessage("Password must be between six and twenty characters long.");
+    } else if (ui->passwordField->text().toStdString() != ui->confirmPasswordField->text().toStdString()) {
+        displayErrorMessage("Passwords do not match.");
+    } else {
+        std::string username = ui->usernameField->text().toStdString();
+        std::string password = ui->passwordField->text().toStdString();
+        std::string address = "127.0.0.1";
+        int port = 5002;
+        LoginResult result;
+        LoqueClient client(address, port);
+        UserType type = (ui->adminCheck->checkState() == Qt::Unchecked) ? UserType::STUDENT :
+                        (ui->adminCheck->checkState() == Qt::Checked) ? UserType::ADMIN : UserType::DNE;
+        Status returnStatus = client.createAccount(username, password, type, result);
+        if(returnStatus == Status::NETWORK_ERR)
+        {
+            displayErrorMessage("Failed to create account due to network problems.");
         }
-        return;
+        else if (returnStatus == Status::DB_ERR)
+        {
+            displayErrorMessage("Failed to create account due to database problems.");
+        }
+        else
+        {
+            emit onRegistration(UserInfo(username, result.userId, result.userType));
+        }
     }
-    emit onRegistration(UserInfo(username, login.userId, login.userType));
+}
+
+void RegisterPane::displayErrorMessage(const char *errorMessage)
+{
+    ui->errorLabel->setText(QString(errorMessage));
+    ui->errorLabel->setStyleSheet("QLabel { color : red }");
+    ui->errorLabel->setMaximumSize(10000, 10000);
+}
+
+void RegisterPane::adminBoxChanged(int state)
+{
+    int width, height;
+    if (state == Qt::Unchecked)
+    {
+        width = 0;
+        height = 0;
+    }
+    else if (state == Qt::Checked)
+    {
+        width = 10000;
+        height = 10000;
+    }
+    ui->adminKeyLabel->setMaximumSize(width, height);
+    ui->adminKeyField->setMaximumSize(width, height);
+}
+
+void RegisterPane::returnToLogin()
+{
+    ui->errorLabel->setMaximumSize(0, 0);
+    ui->errorLabel->clear();
+    ui->usernameField->clear();
+    ui->passwordField->clear();
+    ui->confirmPasswordField->clear();
+    ui->adminCheck->setChecked(false);
+    ui->adminKeyLabel->setMaximumSize(0, 0);
+    ui->adminKeyField->setMaximumSize(0, 0);
+    emit onLoginRequested();
 }
 
 RegisterPane::~RegisterPane()

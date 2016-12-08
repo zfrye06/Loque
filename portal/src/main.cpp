8fa6890f7adf82892d03988750e236972a4fa299
Alex Steele <steeleal123@gmail.com>
2016-12-07 18:50:01 -0700

    portal - Add stylesheet.

--- portal/src/main.cpp
+++ portal/src/main.cpp
@@ -1,11 +1,16 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QFile>
 
 int main(int argc, char *argv[])
 {
+    QFile styles(":/assets/assets/styles.qss");
+    styles.open(QFile::ReadOnly);
+    QString styleString = QLatin1String(styles.readAll());
+
     QApplication a(argc, argv);
+    a.setStyleSheet(styleString);
     MainWindow w;
     w.show();
-
     return a.exec();
 }

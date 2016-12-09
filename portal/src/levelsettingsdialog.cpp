b5a5f3e6de993af6b08e0eedffc482fcfe597dbb
= <bernardcosgriff@gmail.com>
2016-12-09 12:58:19 -0700

    Level settings looks are done but I now hate qt

--- portal/src/levelsettingsdialog.cpp
+++ portal/src/levelsettingsdialog.cpp
@@ -1,25 +1,30 @@
 #include "levelsettingsdialog.h"
 #include "ui_levelsettingsdialog.h"
+#include <QListView>
 
 LevelSettingsDialog::LevelSettingsDialog(std::vector<int> enabledLevels, std::vector<LevelInfo> allLevels, QWidget *parent) :
     QDialog(parent),
     ui(new Ui::LevelSettingsDialog)
 {
     ui->setupUi(this);
+    ui->scrollArea->setWidgetResizable(true);
+    QFrame *frame = new QFrame(ui->scrollArea);
+    ui->scrollArea->setWidget(frame);
+    ui->scrollArea->setVerticalScrollBarPolicy(Qt::ScrollBarAsNeeded);
+    QGridLayout *layout = new QGridLayout(frame);
 
     int row = 0;
     int col = 0;
-
-    QListWidgetItem *item;
     for(size_t i = 0; i < allLevels.size(); i++){
-        item = new QListWidgetItem;
-        ui->levelList->setItemWidget(item, addLevel(allLevels[i], std::find(enabledLevels.begin(), enabledLevels.end(), allLevels[i].id) != enabledLevels.end()));
-//        ui->levelList->  addWidget(new LevelBox(allLevels[i], std::find(enabledLevels.begin(), enabledLevels.end(), allLevels[i].id) != enabledLevels.end(), this), row, col++, 1, 1);
-        if(col > 2){
+        layout->addWidget(addLevel(allLevels[i], std::find(enabledLevels.begin(), enabledLevels.end(), allLevels[i].id) != enabledLevels.end()), row, col++);
+        if(col > 1){
             row++;
             col = 0;
         }
     }
+
+//    layout->addWidget(addLevel(allLevels[0], std::find(enabledLevels.begin(), enabledLevels.end(), allLevels[0].id) != enabledLevels.end()), row, col++);
+    frame->setLayout(layout);
 }
 
 QGroupBox* LevelSettingsDialog::addLevel(LevelInfo lvlInfo, bool enabled){
@@ -33,10 +38,11 @@ QGroupBox* LevelSettingsDialog::addLevel(LevelInfo lvlInfo, bool enabled){
         }
     }
     QLabel *imgLabel = new QLabel;
-    imgLabel->setPixmap(img);
+    imgLabel->setPixmap(img.scaled(250, 250, Qt::KeepAspectRatio));
     QLabel *idLabel = new QLabel(QString::number(lvlInfo.id));
     QLabel *nameLabel = new QLabel(QString::fromStdString(lvlInfo.name));
     QLabel *descriptionLabel = new QLabel(QString::fromStdString(lvlInfo.description));
+    descriptionLabel->setWordWrap(true);
     QPushButton *toggleButton = new QPushButton(enabled ? "Disable Level" : "Enable Level");
 
     vLayout->addWidget(idLabel);

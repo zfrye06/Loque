dc4dd5eb3fde89d37659b3c24037f4f983919802
Alex Steele <steeleal123@gmail.com>
2016-12-09 23:11:01 -0700

    portal - Hook up prototype admin pane.

--- portal/include/adminpane.h
+++ portal/include/adminpane.h
@@ -6,11 +6,8 @@
 #include <QHBoxLayout>
 #include <QPushButton>
 #include <memory>
-
-#include "adminsidebar.h"
 #include "addclassdialog.h"
 #include "../shared/loqueclient.h"
-
 #include "ui_adminpane.h"
 
 class AdminPane : public QWidget
@@ -21,22 +18,17 @@ public:
     explicit AdminPane(UserInfo user, QWidget *parent = 0);
     ~AdminPane();
 
-    void updateClassStats();
-    void setUser(UserInfo user);
-
 signals:
     void classCreated(int classID);
 private:
     UserInfo user;
-
+    std::unique_ptr<std::vector<ClassStats>> allClassStats;
+    int activeClassIdx;
     Ui::AdminPane *ui;
-    QIcon addClassIcon = QIcon(QPixmap(":/assets/assets/add"));
-    QHBoxLayout *mainLayout;
-    QTabWidget *tabs;
-    AdminSidebar *sidebar;
 
-    void addClassTab(const ClassStats& cstats);
     void refreshClassTabs();
+    void classClicked(int);
+    void showCreateClassDialog();
 };
 
 #endif // ADMINPANE_H
--- portal/src/adminpane.cpp
+++ portal/src/adminpane.cpp
@@ -4,28 +4,39 @@
 
 AdminPane::AdminPane(UserInfo user, QWidget *parent) :
     QWidget(parent),
-    ui(new Ui::AdminPane),
-    user(user)
-    // mainLayout(new QHBoxLayout(this)),
-    // tabs(new QTabWidget(this)),
-    // sidebar(new AdminSidebar(user, this))
+    user(user),
+    allClassStats(new std::vector<ClassStats>()),
+    activeClassIdx(-1),
+    ui(new Ui::AdminPane)
 {
     ui->setupUi(this);
-    // mainLayout->addWidget(sidebar);
-    // mainLayout->addWidget(tabs);
-    // setLayout(mainLayout);
-
-    // refreshClassTabs();
-    ui->classList->addItem("CS 3505");
-    ui->classList->addItem("MR. ROMERO");
-    ui->currClassLabel->setText("CS 3505");
-    ui->studentsLabel->setText("Students");
-    ui->studentsList->addItem("John Romero");
-    ui->studentsList->addItem("Lisa Stultz");
-    ui->studentsList->addItem("Bernie (Sanderz)");
-    ui->studentsList->addItem("Alex Dalton Christian");
-
     ui->classList->setAttribute(Qt::WA_MacShowFocusRect, 0);
+
+    connect(ui->classList, &QListWidget::currentRowChanged,
+            this, &AdminPane::classClicked);
+
+    connect(ui->clcreateClassButton, &QPushButton::clicked,
+            this, &AdminPane::showCreateClassDialog);
+
+    connect(ui->ncCreateClassButton, &QPushButton::clicked,
+            this, &AdminPane::showCreateClassDialog);
+
+    connect(ui->exportHtmlButton, &QPushButton::clicked,
+            this, [this] {
+
+    });
+
+    connect(ui->levelSettingsButton, &QPushButton::clicked,
+            this, [this] {
+
+    });
+
+    connect(ui->deleteClassButton, &QPushButton::clicked,
+            this, [this] {
+
+    });
+
+    refreshClassTabs();
 }
 
 AdminPane::~AdminPane() {
@@ -33,26 +44,40 @@ AdminPane::~AdminPane() {
 }
 
 void AdminPane::refreshClassTabs() {
-    std::vector<ClassStats> classStats;
+    std::unique_ptr<std::vector<ClassStats> > newClassStats(new std::vector<ClassStats>);
     LoqueClient client;
-    auto status = client.getAllClassStats(user.userId, classStats);
+    auto status = client.getAllClassStats(user.userId, *newClassStats);
     if (status != Status::OK) {
         // TODO: Show the user that we couldn't get the info.
         std::cerr << "ERROR: Unable to download class stats. " <<
             "Client returned status" << status << std::endl;
         return;
     }
-    tabs->clear();
-    for(auto& cstats : classStats){
-        ClassTab *c = new ClassTab(user.userId, cstats);
-        connect(c, &ClassTab::classCreated, this, &AdminPane::addClassTab);
-        tabs->addTab(c, QString::fromStdString(cstats.className));
+    allClassStats.swap(newClassStats);
+    ui->classList->clear();
+    ui->studentsList->clear();
+    if (allClassStats->size() == 0) {
+        activeClassIdx = -1;
+        ui->stackedWidget->setCurrentWidget(ui->noClassesPage);
+        return;
     }
+    for (auto& classStats : *allClassStats) {
+        ui->classList->addItem(QString::fromStdString(classStats.className));
+    }
+    classClicked(0);
 }
 
-void AdminPane::addClassTab(const ClassStats& cstats) {
-    ClassTab *c = new ClassTab(user.userId, cstats);
-    connect(c, &ClassTab::classCreated, this, &AdminPane::addClassTab);
-    tabs->addTab(c, QString::fromStdString(cstats.className));
-    tabs->setCurrentWidget(c);
+void AdminPane::classClicked(int row) {
+    if (row == -1) return;
+    activeClassIdx = row;
+    const ClassStats& currStats = allClassStats->at(activeClassIdx);
+    ui->studentsList->clear();
+    for (auto& ustats : currStats.studentStats) {
+        ui->studentsList->addItem(QString::fromStdString(ustats.username));
+    }
+    ui->currClassLabel->setText(QString::fromStdString(currStats.className));
+}
+
+void AdminPane::showCreateClassDialog() {
+
 }
